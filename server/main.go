9315b0496a3b31f5ab88812c8e77d557f16e63c5
Lewis Evans <lewisevansuk@gmail.com>
2024-02-26 20:09:14 +0000

    add divide by zero error handling

--- server/main.go
+++ server/main.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/lewidot/doc-grpc/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
@@ -21,6 +23,12 @@ func (s *server) Add(ctx context.Context, in *pb.CalculationRequest) (*pb.Calcul
 }
 
 func (s *server) Divide(ctx context.Context, in *pb.CalculationRequest) (*pb.CalculationResponse, error) {
+	if in.B == 0 {
+		return nil, status.Error(
+			codes.InvalidArgument, "Cannot divide by zero",
+		)
+	}
+
 	return &pb.CalculationResponse{
 		Result: in.A / in.B,
 	}, nil
